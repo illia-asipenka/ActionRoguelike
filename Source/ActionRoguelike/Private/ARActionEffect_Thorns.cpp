fc6788a35a2858959d816813083f9b945c49eb9c
Illia Asipenka <illia.asipenka@gmail.com>
2023-06-12 21:12:18 +0200

    Created Thorns action effect and added as default action.

---
+++ Source/ActionRoguelike/Private/ARActionEffect_Thorns.cpp
@@ -0,0 +1,68 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ARActionEffect_Thorns.h"
+
+#include "ARActionComponent.h"
+#include "ARAttributeComponent.h"
+#include "ARGameplayFunctionLibrary.h"
+
+UARActionEffect_Thorns::UARActionEffect_Thorns()
+{
+	DamageFraction = 0.1f;
+	bAutoStart = true;
+}
+
+void UARActionEffect_Thorns::ReflectDamage(AActor* DamageInstigator, UARAttributeComponent* OwningComponent, float NewHealth, float DamageReceived)
+{
+	if(DamageReceived > 0.0f)
+	{
+		return;
+	}
+	AActor* Owner = GetOwningComponent()->GetOwner();
+
+	if(DamageInstigator == Owner)
+	{
+		return;
+	}
+	
+	float DamageFromThorns = FMath::CeilToInt(-(DamageReceived * DamageFraction));
+
+	if (DamageFromThorns == 0)
+	{
+		return;
+	}
+	
+	UE_LOG(LogTemp, Warning, TEXT("ThornsDamage: %f"), DamageFromThorns);
+
+
+	if(ensure(Owner))
+	{
+		UARGameplayFunctionLibrary::ApplyDamage(Owner, DamageInstigator, DamageFromThorns);
+	}
+}
+
+void UARActionEffect_Thorns::StartAction_Implementation(AActor* Instigator)
+{
+	Super::StartAction_Implementation(Instigator);
+
+	UARAttributeComponent* OwnerAttributes = UARAttributeComponent::GetAttributes(Instigator);
+
+	if(OwnerAttributes)
+	{
+		OwnerAttributes->OnHealthChanged.AddDynamic(this, &UARActionEffect_Thorns::ReflectDamage);
+	}
+	
+}
+
+void UARActionEffect_Thorns::StopAction_Implementation(AActor* Instigator)
+{
+	UARAttributeComponent* OwnerAttributes = UARAttributeComponent::GetAttributes(Instigator);
+
+	if(OwnerAttributes)
+	{
+		OwnerAttributes->OnHealthChanged.RemoveDynamic(this, &UARActionEffect_Thorns::ReflectDamage);
+	}
+
+	Super::StopAction_Implementation(Instigator);
+}
---
+++ Source/ActionRoguelike/Public/ARActionEffect_Thorns.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ARActionEffect.h"
+#include "ARActionEffect_Thorns.generated.h"
+
+class UARAttributeComponent;
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKE_API UARActionEffect_Thorns : public UARActionEffect
+{
+	GENERATED_BODY()
+
+protected:
+	UARActionEffect_Thorns();
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	float DamageFraction;
+
+	UFUNCTION(BlueprintCallable)
+	void ReflectDamage(AActor* DamageInstigator, UARAttributeComponent* OwningComponent, float NewHealth, float DamageReceived);
+
+	virtual void StartAction_Implementation(AActor* Instigator) override;
+	virtual void StopAction_Implementation(AActor* Instigator) override;
+};
